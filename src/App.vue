229f499d7fbc759695cd092accb1dddc6b8d04b4
Tim2.0 <B10023055@yuntech.edu.tw>
2018-06-02 15:44:29 +0800

    Clear Input @keyup.enter

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@
       <h1>todos</h1>
       <input class="new-todo"
         autofocus autocomplete="off"
-        placeholder="What needs to be done?">
+        placeholder="What needs to be done?"
+        v-model="newTodo"
+        @keyup.enter="addTodo">
     </header>
   </div>
 </template>
@@ -14,6 +16,15 @@
 <style src="todomvc-app-css/index.css"></style>
 
 <script>
-export default {}
+export default {
+  data () {
+    return { newTodo: '' }
+  },
+  methods: {
+    addTodo () {
+      this.newTodo = ''
+    }
+  }
+}
 </script>
 /* eslint-disable eol-last */
