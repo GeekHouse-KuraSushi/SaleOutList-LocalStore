e33a07e86c42c328a20502d29e80200fbe52b288
Tim2.0 <B10023055@yuntech.edu.tw>
2018-06-11 10:10:34 +0800

    Pass ESLint Rule When You cmd 'npm run lint'

--- test/e2e/specs/todomvc.spec.js
+++ test/e2e/specs/todomvc.spec.js
@@ -1,48 +1,42 @@
 module.exports = {
-    '應顯示程式標題與輸入欄位'(browser) {
-    const devServer = browser.globals.devServerURL
-        browser
-            .page.todomvc().show()
-            .page.todomvc().shouldSeeTitleAndInput()
-            .end();
-    },
-    '輸入欄位應該在輸入代辦事項後清空'(browser) {
-        const devServer = browser.globals.devServerURL;
-        const todo = 'This is new todo'
-            browser
-                .page.todomvc().show()
-                .setValue('.new-todo',[todo,browser.Keys.ENTER])
-                .assert.value('.new-todo','')
-                .end();
-        },
-    '新增測試'(browser){
-        const devServer = browser.globals.devServerURL;
-        const  todo = 'This is new todo';
-        browser
-        .page.todomvc().show()
-        .setValue('.new-todo',[todo,browser.Keys.ENTER])
-        .waitForElementVisible('.todo-list > .todo:first-child',1000)
-        .assert.containsText('.todo-list > .todo:first-child > .view > label', todo)
-        .end();
-        
-    },
-    '沒新增列表隱藏'(browser){
-        const devServer = browser.globals.devServerURL;
-        browser
-        .page.todomvc().show()
-        .assert.hidden('.main',1000)
-        .end();
-    },
-    '刪除事項'(browser){
-        const devServer = browser.globals.devServerURL;
-        const  todo = 'This is new todo';
-        browser
-        .page.todomvc().show()
-        .setValue('.new-todo',[todo,browser.Keys.ENTER])
-        .waitForElementVisible('.todo-list > .todo:first-child',1000)
-        .assert.containsText('.todo-list > .todo:first-child > .view > label', todo)
-        .page.todomvc().deleteTodo()
-        .page.todomvc().shouldGetEmptyTodoList()
-        .end();
-    },
+  '應顯示程式標題與輸入欄位' (browser) {
+    browser
+      .page.todomvc().show()
+      .page.todomvc().shouldSeeTitleAndInput()
+      .end()
+  },
+  '輸入欄位應該在輸入代辦事項後清空' (browser) {
+    const todo = 'This is new todo'
+    browser
+      .page.todomvc().show()
+      .setValue('.new-todo', [todo, browser.Keys.ENTER])
+      .assert.value('.new-todo', '')
+      .end()
+  },
+  '新增測試' (browser) {
+    const todo = 'This is new todo'
+    browser
+    .page.todomvc().show()
+    .setValue('.new-todo', [todo, browser.Keys.ENTER])
+    .waitForElementVisible('.todo-list > .todo:first-child', 1000)
+    .assert.containsText('.todo-list > .todo:first-child > .view > label', todo)
+    .end()
+  },
+  '沒新增列表隱藏' (browser) {
+    browser
+      .page.todomvc().show()
+      .assert.hidden('.main', 1000)
+      .end()
+  },
+  '刪除事項' (browser) {
+    const todo = 'This is new todo'
+    browser
+      .page.todomvc().show()
+      .setValue('.new-todo', [todo, browser.Keys.ENTER])
+      .waitForElementVisible('.todo-list > .todo:first-child', 1000)
+      .assert.containsText('.todo-list > .todo:first-child > .view > label', todo)
+      .page.todomvc().deleteTodo()
+      .page.todomvc().shouldGetEmptyTodoList()
+      .end()
+  }
 }
